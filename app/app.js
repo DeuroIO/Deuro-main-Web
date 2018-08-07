4dcc599cabcf461e601c87edeb8836cf4cb857c2
ekrantiservices <info@ekrantiservices.com>
2018-08-07 19:38:45 +0530

    work on home page

--- app/app.js
+++ app/app.js
@@ -2,5 +2,24 @@ var myApp = angular.module("myModule",[]);
 
 myApp.controller("myController", function($scope)
 {
-    $scope.message = "Angular JS Tutorial";
+// $scope.showLoader = true;
+
+    $scope.myvalue = false;
+    
+    angular.element(function () {
+        $scope.showLoader = false;   
+    });
+
+    $scope.toggleClass = function (event){
+       if($scope.myvalue == false)
+       {
+        $scope.myvalue = true;  
+       }
+       else{
+        $scope.myvalue =  false;  
+       }
+     
+    }
+
 });
+
